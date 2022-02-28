1977868c6abbf4845fd9a43e5023fff4e1dc6041
Elizabeth <elizabethtreahy@gmail.com>
2022-02-27 22:29:50 -0500

    added javascript to modify name color

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const h2 = document.createElement("h2");
-h2.textContent = "";
-document.querySelector("body").appendChild(h2);
+// const h2 = document.createElement("h2");
+// h2.textContent = "";
+// document.querySelector("body").appendChild(h2);
+
+let name = document.getElementById("name-header");
+
+name.addEventListener("mouseenter", function( event ) {
+  event.target.style.color = "black"; 
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
